25d92e48fc11d078946195ec541e9b1701cd64ed
Alekander Bialik <aleksanderbialik97@gmail.com>
2021-10-01 00:34:10 +0200

    Adjusted exchange calculations

--- src/main/java/com/example/demo/account/service/AccountService.java
+++ src/main/java/com/example/demo/account/service/AccountService.java
@@ -5,9 +5,12 @@ import com.example.demo.account.model.AccountEntity;
 import com.example.demo.services.nbp.model.Rates;
 import com.example.demo.services.nbp.service.ExchangeRatesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.UUID;
 
 @Service
@@ -27,17 +30,22 @@ public class AccountService {
         return accountRepository.getAccountById(id);
     }
 
-    public void exchangeToPln(AccountEntity entity, BigDecimal amount){
+    public ResponseEntity<Object> exchangeToPln(AccountEntity entity, BigDecimal amount){
         Rates rates = exchangeRatesService.getExchangeRates();
+        MathContext m = new MathContext(4);
         entity.setUsdAccount(entity.getUsdAccount().subtract(amount));
-        entity.setPlnAccount(entity.getPlnAccount().add(amount.multiply(rates.getBid())));
+        entity.setPlnAccount(entity.getPlnAccount().add(amount.multiply(rates.getBid(),m)));
         accountRepository.save(entity);
+        return ResponseEntity.status(200).body("{\"message\":\"USD was successfully exchanged to PLN\"}");
     }
 
-    public void exchangeToUsd(AccountEntity entity, BigDecimal amount){
+    public ResponseEntity<Object> exchangeToUsd(AccountEntity entity, BigDecimal amount){
         Rates rates = exchangeRatesService.getExchangeRates();
+        MathContext m = new MathContext(4);
         entity.setPlnAccount(entity.getPlnAccount().subtract(amount));
-        entity.setUsdAccount(entity.getUsdAccount().add(amount.divide(rates.getAsk())));
+        entity.setUsdAccount(entity.getUsdAccount().add(amount.divide(rates.getAsk(), m)));
         accountRepository.save(entity);
+
+        return ResponseEntity.status(200).body("{\"message\":\"PLN was successfully exchanged to USD\"}");
     }
 }
