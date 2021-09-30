1d68d24d765ea1d0da8cc329c8eabd47b7eef861
Alekander Bialik <aleksanderbialik97@gmail.com>
2021-09-30 18:47:01 +0200

    Added AccountEntity in account creation pipeline

---
+++ src/main/java/com/example/demo/account/model/AccountEntity.java
@@ -0,0 +1,35 @@
+package com.example.demo.account.model;
+
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity(name = "Account")
+@Table(name = "account")
+@Data
+public class AccountEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private BigDecimal plnAccount;
+
+    @Column
+    private BigDecimal usdAccount;
+
+
+}
